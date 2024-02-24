c64fa515e8489f435389b80044e0867b1968c602
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-02-24 15:00:26 -0300

    history page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,80 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>When</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statuscolor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statuscolor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statuscolor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statuscolor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statuscolor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statuscolor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutos</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statuscolor="green">Completed</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
