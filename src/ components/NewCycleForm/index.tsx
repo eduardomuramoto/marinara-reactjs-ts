644f67126845b4bf9c0f6efbe097b59ac2315370
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-02-28 20:58:20 -0300

    add cycle

--- src/ components/NewCycleForm/index.tsx
+++ src/ components/NewCycleForm/index.tsx
@@ -1,7 +1,7 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
-import { useContext } from 'react'
-import { CyclesContext } from '../../pages/Home'
+import { CyclesContext } from '../../contexts/CyclesContext'
 import { useFormContext } from 'react-hook-form'
+import { useContext } from 'react'
 export function NewCycleForm() {
   const { activeCycle } = useContext(CyclesContext)
   const { register } = useFormContext()
